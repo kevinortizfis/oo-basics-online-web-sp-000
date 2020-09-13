540b0607d5c37496a7bfece20e5c54d63d5591ce
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-13 23:32:50 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,10 @@
 class Shoe
+
   def initialize(brand,color=unknown,size=unknown,material=unknown,condition=unknown)
+    @brand = brand
+    @color = color
+    @size = size
+    @material = material
+    @condition = condition
+  end
 end
